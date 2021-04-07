ffcfde6fb7aef9796164f985393caa051b27fbce
Jaaaaack <30641133+Jaaaaack@users.noreply.github.com>
2021-04-07 13:56:39 +0100

    Project Almost Finished
    Added css and more interactivity

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 console.log("Hello!");
 
+let viz;
+
 // To do list:
 // 1. Grab URL of dashboard and store it in a variable
 
@@ -14,14 +16,81 @@ const vizContainer = document.getElementById("vizContainer");
 
 const options = {
   device: "desktop",
+  Category: ["Technology", "Office Supplies"],
+  onFirstInteractive: function () {
+    console.log("Viz is ready!");
+    document.getElementById("minValue").disabled = false;
+    document.getElementById("maxValue").disabled = false;
+    document.getElementById("filterButton").disabled = false;
+  },
 };
 
 // 3. Create a fuction to call the Tableau JS API
 
 function initViz() {
-  const viz = new tableau.Viz(vizContainer, vizUrl, options);
+  viz = new tableau.Viz(vizContainer, vizUrl, options);
 }
+
+// Grab the pdf button
+
+const pdfButton = document.getElementById("pdfButton");
+function generatePDF() {
+  console.log("generating PDF...");
+  viz.showExportPDFDialog();
+}
+
+pdfButton.addEventListener("click", generatePDF);
+
+// Grab the PowerPoint button
+
+const ppButton = document.getElementById("ppButton");
+
+function generatePP() {
+  console.log("generating PowerPoint...");
+  viz.showExportPowerPointDialog();
+}
+
+ppButton.addEventListener("click", generatePP);
+
+// grab swap viz button
+
+const swapViz = document.getElementById("swapViz");
+
+function switchViz() {
+  console.log("Changing the dashboard...");
+
+  const otherVizUrl =
+    "public.tableau.com/views/BostonFemaleWheelchairChamps/BostonFemaleWheelchairChamps?:language=en-GB&:display_count=y&mobile=&:origin=viz_share_link";
+}
+
+// enable filter button
+
+// fitler dashboard
+
+function getRangeValues() {
+  console.log("Filtering the dashboard...");
+  const minValue = document.getElementById("minValue").value;
+  const maxValue = document.getElementById("maxValue").value;
+  console.log(minValue, maxValue);
+
+  // get the workbook
+  const workbook = viz.getWorkbook();
+  const activeSheet = workbook.getActiveSheet();
+  const sheets = activeSheet.getWorksheets();
+  const sheetToFilter = sheets[1];
+  sheetToFilter
+    .applyRangeFilterAsync("Sales", {
+      min: minValue,
+      max: maxValue,
+    })
+    .then(console.log("Filter applied!"));
+  console.log(sheets);
+}
+
+document
+  .getElementById("filterButton")
+  .addEventListener("click", getRangeValues);
 
 // 4. Call that function
 
-document.addEventListener("DOMContentLoaded", initViz());
+https: document.addEventListener("DOMContentLoaded", initViz());
